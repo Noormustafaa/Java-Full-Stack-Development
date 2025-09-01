138222dceb8b6178956fd55d3313d02a71b47787
Noor Mustafa Rajpar <150359415+Noormustafaa@users.noreply.github.com>
2025-09-02 00:17:50 +0500

    Added_Swapping_Two_Numbers_UsingThirdVariable
    Day1

---
+++ 120_Programs_Challange/Swapping_Two_Number.java
@@ -0,0 +1,28 @@
+
+package pkg120_programs_in_java;
+
+
+public class Swapping_Two_Number {
+
+   
+    public static void main(String[] args) {
+        
+        
+        
+       //Logic 1 Using Third Variable 
+       
+       int a=20;
+       int b=30;
+       
+        System.out.println("Before_Swapping value of a:"+a+" and   b:"+ b );
+       
+       int t=20;
+       
+       a=b; //so here a=30 now
+       b=t; // and now b= here 20
+    
+     System.out.println("After_Swapping value of a:"+a+" and   b:"+ b );
+       
+    }
+    
+}
